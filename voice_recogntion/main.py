696e2df62c9eae0a53c4f0f759c7f849d75dd32f
Tom Selfin <selfint@gmail.com>
2021-09-01 19:15:10 +0300

    FEAT: add main file

---
+++ voice_recogntion/main.py
@@ -0,0 +1,27 @@
+from collections import deque
+from pathlib import Path
+from voice_recogntion.sox_recorder import SoxRecorder
+import tempfile
+import time
+
+
+def main():
+    audio_queue = deque()
+
+    with tempfile.TemporaryDirectory() as audio_dir:
+        audio_path = Path(audio_dir)
+
+        sr = SoxRecorder(audio_path, audio_queue)
+
+        sr.start_watchdog()
+        sr.start_sox_subprocess()
+
+        time.sleep(10)
+        sr.stop_sox_subprocess()
+        sr.stop_watchdog()
+
+    print(list(audio_queue))
+
+
+if __name__ == "__main__":
+    main()
