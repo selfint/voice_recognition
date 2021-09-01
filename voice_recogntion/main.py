d9cb52ba9ee8642338a1d5ea0e96b81d86d8a975
Tom Selfin <selfint@gmail.com>
2021-09-01 20:31:02 +0300

    FEAT: improve main to record until KeyboardInterrupt received

--- voice_recogntion/main.py
+++ voice_recogntion/main.py
@@ -2,24 +2,35 @@ import tempfile
 import time
 from collections import deque
 from pathlib import Path
+from typing import Deque
 
 from voice_recogntion.sox_recorder import SoxRecorder
+
+
+def start_sox(audio_dir: Path, audio_queue: Deque[Path]):
+    """Start the SoxRecorder.
+
+    Args:
+        audio_dir (Path): Directory to generate audio files in
+        audio_queue (Deque[Path]): Queue to push generated audio files into
+    """
+    sr = SoxRecorder(Path(audio_dir), audio_queue)
+    sr.start()
+
+    return sr
 
 
 def main():
     audio_queue = deque()
 
     with tempfile.TemporaryDirectory() as audio_dir:
-        sr = SoxRecorder(Path(audio_dir), audio_queue)
+        sr = start_sox(Path(audio_dir), audio_queue)
+        try:
+            while True:
+                time.sleep(1)
 
-        sr.start_watchdog()
-        sr.start_sox_subprocess()
-
-        time.sleep(10)
-        sr.stop_sox_subprocess()
-        sr.stop_watchdog()
-
-    print(list(audio_queue))
+        except KeyboardInterrupt:
+            sr.stop()
 
 
 if __name__ == "__main__":
