c5b06de01d7196703877df8af46f46952873650c
Tom Selfin <selfint@gmail.com>
2021-09-01 09:36:01 +0300

    FEAT: add SoxRecorder

--- tests/test_voice_recogntion.py
+++ tests/test_voice_recogntion.py
@@ -1,5 +1,13 @@
 from voice_recogntion import __version__
+from voice_recogntion.main import SoxRecorder
+from unittest.mock import patch, MagicMock
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@patch("subprocess.Popen")
+def test_sox_recording_subprocess(mock_popen: MagicMock):
+    SoxRecorder().start_sox_subprocess()
+    mock_popen.assert_called_once()
