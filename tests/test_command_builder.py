2bc329ebb8c44f34868e979dabf5ed8ed84f389c
Tom Selfin <selfint@gmail.com>
2021-09-03 19:17:05 +0300

    FEAT: add command extraction using keywords

---
+++ tests/test_command_builder.py
@@ -0,0 +1,17 @@
+from voice_recognition.command_builder import CommandBuilder
+
+
+def test_build_command():
+    texts = [
+        "start command",
+        "random test text",
+        "that has a command",
+        "end command"
+    ]
+
+    expected_command = "random test text that has a command"
+
+    cb = CommandBuilder("start command", "end command")
+    command = cb.get_commands_in_texts(texts)
+
+    assert command == [expected_command]
---
+++ voice_recognition/command_builder.py
@@ -0,0 +1,49 @@
+from typing import List, Optional
+import re
+
+
+class CommandBuilder():
+    def __init__(self, start_kw: str, end_kw: str) -> None:
+        self._start_kw = start_kw
+        self._end_kw = end_kw
+
+    def get_commands_in_texts(self, texts: List[str]) -> List[str]:
+        """Get all commands in texts.
+
+        A "command" is the content between the start_kw and the end_kw.
+        All commands found will be returned in a list.
+
+        Args:
+            texts: Texts to build command from
+
+        Returns:
+            List[str]: All commands in texts
+        """
+
+        full_text = " ".join(texts)
+
+        # find commands start and end
+        full_text = full_text.replace(self._start_kw, "<<<")
+        full_text = full_text.replace(self._end_kw, ">>>")
+
+        # find all commands using regex
+        regex = re.compile("<<<(.*)>>>")
+
+        commands = regex.findall(full_text)
+
+        commands = [self._process_cmd(cmd) for cmd in commands]
+
+        return commands
+
+    def _process_cmd(self, cmd: str) -> str:
+        """Process command string.
+
+        Args:
+            cmd: Command string to process
+
+        Returns:
+            str: Processed command
+        """
+
+        return cmd.strip()
+
